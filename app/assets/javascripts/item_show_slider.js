0669c18a1423497813c1d4a7cbcccd828034b49a
TomInJpn <66042540+TomInJpn@users.noreply.github.com>
2020-10-30 16:42:27 +0900

    item show slider 見直し

--- app/assets/javascripts/item_show_slider.js
+++ app/assets/javascripts/item_show_slider.js
@@ -2,7 +2,7 @@ $(function(){
 
   $(".itemShow").on("mouseover click",".itembox__body--thumbnail div",
     function(){
-      $(".itembox__body--slider img").css("transform",`translateX(${-100 * $(this).data("index")}%)`);
+      $(".itembox__body--slider__img").css("transform",`translateX(${-100 * $(this).data("index")}%)`);
     }
   );
 
