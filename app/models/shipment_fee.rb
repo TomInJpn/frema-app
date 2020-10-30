9ba5457a16a70fb3e11ae6f41ab2bb8103c808e7
TomInJpn <66042540+TomInJpn@users.noreply.github.com>
2020-10-31 08:49:05 +0900

    Update shipment_fee.rb

--- app/models/shipment_fee.rb
+++ app/models/shipment_fee.rb
@@ -1,6 +1,6 @@
 class ShipmentFee < ActiveHash::Base
   self.data = [
-      {id: 1, name: '送料込み（出品者負担）'},
-      {id: 2, name: '着払い（購入者負担）'}
+      {id: 1, name: '送料込（出品者負担）'},
+      {id: 2, name: '送料別（購入者負担）'}
   ]
 end
