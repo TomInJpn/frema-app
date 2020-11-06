3dda060dfebb9cef3f68c8a59a2cfe15628c2b77
TomInJpn <66042540+TomInJpn@users.noreply.github.com>
2020-11-06 17:17:57 +0900

    nginX設定

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,7 @@ working_directory app_path
 # stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 # stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 pid "#{app_path}/tmp/pids/unicorn.pid"
-listen 3000
+listen "#{app_path}/tmp/sockets/unicorn.sock"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
