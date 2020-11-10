f43d2d69a404f2e76717b35f0c30d6df988eb0f4
TomInJpn <66042540+TomInJpn@users.noreply.github.com>
2020-11-10 19:14:50 +0900

    コメントアウト追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,7 +22,7 @@ class Item < ApplicationRecord
   has_many :payments
 
   has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images,allow_destroy: true
+  accepts_nested_attributes_for :images,allow_destroy: true#, limit:10
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :condition
