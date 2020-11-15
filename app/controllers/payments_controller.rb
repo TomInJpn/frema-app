52afab30f132dfad04630ab321028423efeff57c
TomInJpn <66042540+TomInJpn@users.noreply.github.com>
2020-11-15 22:05:47 +0900

    transaction検証中

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -29,14 +29,16 @@ class PaymentsController < ApplicationController
 
       payment.save!
       item.update!(stock: item.stock)
+      redirect_to root_path
     rescue
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      customer = Payjp::Customer.retrieve(current_user.card.customer_id) if current_user.card
-      @card = customer.cards.retrieve(current_user.card.card_id) if current_user.card
-      @item = Item.find(params[:item_id])
-      @shipment_fee = ShipmentFee.find(@item.shipment_fee_id)
-      @postage = Postage.find(@item.user.address.prefecture_id) if @shipment_fee.id == 2
-      render "new"
+      # Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      # customer = Payjp::Customer.retrieve(current_user.card.customer_id) if current_user.card
+      # @card = customer.cards.retrieve(current_user.card.card_id) if current_user.card
+      # @item = Item.find(params[:item_id])
+      # @shipment_fee = ShipmentFee.find(@item.shipment_fee_id)
+      # @postage = Postage.find(@item.user.address.prefecture_id) if @shipment_fee.id == 2
+      # render "new"
+      redirect_to new_item_payment_path
     end
   end
 
